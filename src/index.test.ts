f5aa73d5bbf09aa0ded737fe13b145671bf58af5
Ben Chilcott <38051283+bchilcott@users.noreply.github.com>
2023-05-30 08:55:34 +0100

    Simplify test

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,6 +3,7 @@ import { add } from "./index.js";
 
 describe("A collection of tests", () => {
   it("should add numbers", () => {
-    expect(add(1, 1)).toBe(2);
+    const result = add(1, 1);
+    expect(result).toBe(2);
   });
 });
